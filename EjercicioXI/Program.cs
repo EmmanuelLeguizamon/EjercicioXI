9f58ff2328f9f557b0f4a532002df9b0218dcdb2
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-07 22:50:19 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioXI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace EjercicioXI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+
+            {
+
+                Console.WriteLine("Ingrese una frase a continuación:");
+
+                string ingreso = Console.ReadLine().ToLower();
+
+
+
+                if (ingreso.Contains('á') || ingreso.Contains("é") || ingreso.Contains("í") || ingreso.Contains("ó") || ingreso.Contains("ú"))
+
+                {
+
+                    ingreso = ingreso.Replace("é", "e");
+
+                    ingreso = ingreso.Replace("í", "i");
+
+                    ingreso = ingreso.Replace("ó", "o");
+
+                    ingreso = ingreso.Replace("ú", "u");
+
+                    ingreso = ingreso.Replace("á", "a");
+
+
+
+                    Console.WriteLine("\nSu frase sin acentos es: " + ingreso);
+
+                }
+
+                else
+
+                    Console.WriteLine("\nNo se han ingresado vocales acentuadas");
+
+
+
+                Console.ReadKey();
+
+
+
+            } while (true);
+        }
+    }
+}
